22a7435efdc637f6879f13d4bb19cc7705ef19ae
Ashutoshnext <140349306+Ashutoshnext@users.noreply.github.com>
2023-07-26 12:31:38 +0530

    Add files via upload

---
+++ sket.py
@@ -0,0 +1,3 @@
+from sketchpy import library as ans
+res = ans.apj()
+res.draw()
---
+++ skte2.py
@@ -0,0 +1,6 @@
+from sketchpy import  canvas
+# ans='F:\\photo\\mahadev.jpg'
+ans=input('Enter Your Image Path')
+res=canvas.sketch_from_image(ans)
+res.draw() 
+
